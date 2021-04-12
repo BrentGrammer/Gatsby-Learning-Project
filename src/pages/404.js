6ae83d505da6eced2a83ccb02a4b087e09425f82
Brent Marquez <brentonmarquez@gmail.com>
2021-04-11 20:22:51 -0600

    Custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404 Custom page</h2>
+      <p>Make a 404.js file in your pages folder to customize the 404 page.</p>
+    </div>
+  )
+}
