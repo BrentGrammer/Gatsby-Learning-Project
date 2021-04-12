c69f5c5980689141018f72edd0e7ed5f89bf9d38
Brent Marquez <brentonmarquez@gmail.com>
2021-04-11 21:31:45 -0600

    Adding metadata we can query to our site manually

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "My gatsby site",
+    description: "Learning Gatsby",
+    copyright: "This is the copyright message copyright 2021",
+  },
 }
