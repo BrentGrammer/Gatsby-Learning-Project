9a401dc9024ee171a3fd95bdb839e54bd153da13
Brent Marquez <brentonmarquez@gmail.com>
2021-04-11 20:21:24 -0600

    ROUTING - creating index.js in folder

---
+++ src/pages/projects/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+/**
+ *
+ * If you create a index.js file in a directory, going to the folder name as a route will take you to the component.
+ * ex: /projects  renders this component.
+ */
+
+export default function Projects() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <h3>Projects I've Created</h3>
+    </div>
+  )
+}
